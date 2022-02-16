c1fdb34176048fac324fb839257ee0ea0d79fe9f
Tanishq Chugh <85037220+tanishqchugh2002@users.noreply.github.com>
2022-02-16 14:07:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings: ["", "Coder", "Web Developer", "UI/UX Designer", "App Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
